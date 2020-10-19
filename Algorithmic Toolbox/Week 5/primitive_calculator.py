8cc873c9641cfbe41af86519047aeef068e6599f
Shantanu Z <51382138+7shantanu7@users.noreply.github.com>
2020-10-19 12:41:20 +0530

    Add files via upload

---
+++ Algorithmic Toolbox/Week 5/primitive_calculator.py
@@ -0,0 +1,32 @@
+import sys
+def optimal_sequence(n):
+    sequence = []
+        
+    a = [0]*(n+1)
+    
+    for each in range(1, len(a)):
+        a[each] = a[each-1] + 1
+        if each % 2 == 0:
+            a[each] = min(1+a[each//2], a[each])
+        if each % 3 == 0:
+            a[each] = min(1+a[each//3], a[each])
+    
+    while n > 1:
+        sequence.append(n)
+        if a[n-1] == a[n]-1:
+            n = n - 1
+        elif (n % 2 == 0 and a[n//2] == a[n]-1):
+            n = n//2     
+        elif (n % 3 == 0 and a[n//3] == a[n]-1):
+            n = n//3
+    
+    sequence.append(1)
+    
+    return reversed(sequence)
+
+input = sys.stdin.read()
+n = int(input)
+sequence = list(optimal_sequence(n))
+print(len(sequence) - 1)
+for x in sequence:
+    print(x, end=' ')
